4c39bf89fb993459083bfbb8ca6795b37b3b5cdd
Douglas Adam Smith II <iihtimsad@gmail.com>
2013-09-04 15:32:15 -0400

    Quick patch.
    I had typed in the file paths incorrectly

--- package.js
+++ package.js
@@ -4,12 +4,8 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('client', 'server');
-    api.add_files('lib/client.js', 'client');
-    api.add_files('lib/startup.js', 'client');
-    api.add_files('lib/manager.js', 'server');
-    
-    if (typeof api.export !== 'undefined') {
-   		api.export(['Client'], ['client']);
-    }
+    api.add_files('lib/client/client.js', 'client');
+    api.add_files('lib/client/startup.html', 'client');
+    api.add_files('lib/server/manager.js', 'server');
 });
 
