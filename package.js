1e9d836722af4d2bac7ecfb1b4fabb8a4bc5ec2f
Douglas Adam Smith II <iihtimsad@gmail.com>
2013-09-04 16:24:17 -0400

    Added api.export

--- package.js
+++ package.js
@@ -6,5 +6,7 @@ Package.on_use(function (api) {
     api.use();
     api.add_files('lib/client/client.js', 'client');
     api.add_files('lib/server/manager.js', 'server');
+    if (api.export)
+  		api.export('Client');
 });
 
