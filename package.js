acf7062b1d12a668e42b193edffb10551413f222
Douglas Adam Smith II <iihtimsad@gmail.com>
2013-09-04 15:37:02 -0400

    Testing

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use('client', 'server');
+    api.use('server');
     api.add_files('lib/client/client.js', 'client');
     api.add_files('lib/client/startup.html', 'client');
     api.add_files('lib/server/manager.js', 'server');
