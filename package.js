c023c1abae520caabbef38bb8c3c7c6338d9fa1b
Douglas Adam Smith II <iihtimsad@gmail.com>
2013-09-04 15:42:24 -0400

    Tightened code.

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use('server');
+    api.use();
     api.add_files('lib/client/client.js', 'client');
     api.add_files('lib/client/startup.html', 'client');
     api.add_files('lib/server/manager.js', 'server');
