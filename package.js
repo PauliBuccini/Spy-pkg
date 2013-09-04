5a0a0ad87dbf4b653c5aca20f4cdf0c0ec9c47da
Douglas Adam Smith II <iihtimsad@gmail.com>
2013-09-04 16:19:35 -0400

    Removed unneeded files.

--- package.js
+++ package.js
@@ -5,7 +5,6 @@ Package.describe({
 Package.on_use(function (api) {
     api.use();
     api.add_files('lib/client/client.js', 'client');
-    api.add_files('lib/client/startup.html', 'client');
     api.add_files('lib/server/manager.js', 'server');
 });
 
